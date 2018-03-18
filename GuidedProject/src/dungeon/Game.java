079f37ef496478de6d443444b5fe582bdb124f64
leonormfreitas <leonormendesfreitas@gmail.com>
2018-03-18 15:31:33 +0000

    ogre done

--- GuidedProject/src/dungeon/Game.java
+++ GuidedProject/src/dungeon/Game.java
@@ -1,13 +1,16 @@
 package dungeon;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
+	
+	static int flag=0;
 
-	public static void printBoard(char[][] board) {
+	public static void printBoard(char[][] board, int size) {
 		System.out.println();
-		for(int i = 0; i < 10; i++)
+		for(int i = 0; i < size; i++)
 		{
-			for(int j = 0; j < 10; j++)
+			for(int j = 0; j < size; j++)
 			{
 				System.out.print(board[i][j]);
 				System.out.print("  ");
@@ -18,13 +21,13 @@ public class Game {
 	}
 
 
-	public static int[] getPos(char[][] board){
+	public static int[] getPos(char[][] board, char mover){
 		int[] pos = new int[2];
 		for(int i = 0; i < 10; i++)
 		{
 			for(int j = 0; j < 10; j++)
 			{
-				if(board[i][j]=='H' || board[i][j] == 'K')
+				if(board[i][j]==mover)
 				{
 					pos[0] = i; 
 					pos[1] = j;
@@ -35,7 +38,7 @@ public class Game {
 	}
 
 
-	public static void checkDirection(char input, int[] pos, char[][]board)
+	public static void checkDirection(char input, int[] pos, char[][]board, char mover)
 	{
 		int x= pos[0];
 		int y= pos[1];
@@ -44,59 +47,44 @@ public class Game {
 		if(input == 'A' || input == 'a')
 		{	
 			if(board[x][y-1]==' '){
-				board[x][y-1]='H';
+				board[x][y-1]=mover;
 				board[x][y]=' ';
 			}
-			else
-				if(atLever(x,y-1, board))
-				{
-					board[x][y-1]='K';
-					board[x][y]=' ';
-				}
 
 		}
 		if(input == 'S' || input == 's')
 		{
 			if(board[x+1][y]==' '){
-				board[x+1][y]='H';
+				board[x+1][y]=mover;
 				board[x][y]=' ';
 			}
-			else
-				if(atLever(x+1,y, board))
-				{
-					board[x+1][y]='K';
-					board[x][y]=' ';
-				}
-
+		
 		}
 		if(input == 'W' || input == 'w')
 		{
 			if(board[x-1][y]==' '){
-				board[x-1][y]='H';
+				board[x-1][y]=mover;
 				board[x][y]=' ';
 			}
-			else
-				if(atLever(x-1,y, board))
-				{
-					board[x-1][y]='K';
-					board[x][y]=' ';
-				}
 
 		}
 		if(input == 'D' || input == 'd')
 		{
 			if(board[x][y+1]==' '){
-				board[x][y+1]='H';
+				board[x][y+1]=mover;
 				board[x][y]=' ';
 			}
+	
+		}
+		if(input == 'T' || input == 't') {
+			if(atLever(x,y, board))
+			{
+				board[5][0]='S';
+				board[6][0]='S';
+				flag=1;
+			}
 			else
-				if(atLever(y+1,y, board))
-				{
-					board[x+1][y]='K';
-					board[x][y]=' ';
-				}
-
-
+				System.out.println("Nothing to do, try again!!");
 		}
 
 	}
@@ -117,29 +105,66 @@ public class Game {
 		};
 		// TODO: lever esta a desaparecer, why? 
 		// movimentos do guarda
+		
+		char[][] board2 = new char [][]{
+			{'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'},
+			{'I', ' ', ' ', ' ', 'O', ' ', ' ', 'k', 'X'},
+			{'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+			{'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+			{'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+			{'X', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', 'X'},
+			{'X', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', 'X'},
+			{'X', 'H', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+			{'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'}
+
+		};
 
 		Scanner key = new Scanner(System.in);
 		char key2 = '1';
+		
+		int count=0;
 
-
+		printBoard(board, 10);
+		
 		while (true) 
 		{
-			printBoard(board);
 			key2 = key.next().charAt(0);
 
 			if(key2 == 'q' || key2 == 'Q') // Para sair TODO: ver porque esta a dar erro ao pressionar esta tecla
 				break;
 
 
-			int pos[] = getPos(board);
+			int pos[] = getPos(board, 'H');
+			
+			checkDirection(key2, pos, board, 'H');
+			
+			pos = getPos(board, 'H');
+			
+			guardMovement(board, count);
+			
 			if(adjacentGuard(pos[0], pos[1], board))
 			{
+				printBoard(board, 10);
 				System.out.println("GAME OVER!");
 				key.close();
 				return;
 			}
-			checkDirection(key2, pos, board);
 			
+			if(count==23) {
+				count=0;
+			}else
+				count++;
+			
+			printBoard(board, 10);
+			
+			if(((pos[0]==5 && pos[1]==1) || (pos[0]==6 && pos[1]==1)) && flag==1) {
+				while(true) {
+					ogreMovement(board2); 
+					printBoard(board2, 9);
+				}
+				
+			}
+					
 
 		}
 		key.close();
@@ -150,7 +175,7 @@ public class Game {
 
 	public static boolean atLever(int x, int y, char[][]board)
 	{
-		if(board[x][y] == 'k')
+		if(board[x-1][y] == 'k' || board[x+1][y] == 'k' || board[x][y-1] == 'k' || board[x][y+1] == 'k')
 			return true;
 		else
 			return false;
@@ -168,6 +193,41 @@ public class Game {
 			return true;
 		else
 			return false;
+	}
+	
+	public static void guardMovement(char[][]board, int count) {
+		int pos[]= getPos(board, 'G');
+		char moves[] = {'A','S','S','S','S','A','A','A','A','A','A','S','D','D','D','D','D','D','D','W','W','W','W','W'};
+		checkDirection(moves[count], pos, board, 'G');
+	}
+	
+	public static void ogreMovement(char[][]board2) {
+		Random rand = new Random();
+		
+		char move=' ';
+		
+		int num_move=rand.nextInt(4) + 1;
+		
+		switch(num_move) {
+			case 1:
+				move='A';
+				break;
+			case 2:
+				move='S';
+				break;
+			case 3:
+				move='W';
+				break;
+			case 4:
+				move='D';
+				break;
+		
+		}
+		
+		int pos[]= getPos(board2, 'O');
+		
+		checkDirection(move, pos, board2, 'O');
+		
 	}
 
 
