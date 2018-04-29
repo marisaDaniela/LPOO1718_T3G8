45045f9e33724ac6878f72c0b82e6f7b57de6528
Marisa Oliveira <filipinos.tunafe@gmail.com>
2018-04-29 12:12:01 +0100

    deleted old project

--- DefendingVillage/src/elements/Warrior.java
+++
@@ -1,9 +0,0 @@
-package elements;
-
-public class Warrior extends Entity {
-
-	public Warrior() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- DefendingVillage/src/elements/Wizard.java
+++
@@ -1,9 +0,0 @@
-package elements;
-
-public class Wizard extends Entity {
-
-	public Wizard() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
