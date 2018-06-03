ca3a3fb962a04d1a90ad687c52889729da637119
Marisa Oliveira <up201308594@fe.up.pt>
2018-06-03 21:02:05 +0100

    Update DesktopLauncher.java

--- SuperMario/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ SuperMario/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ import com.mygdx.SuperMario;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1200;
-		config.height = 624;
+		config.width = 2000;
+		config.height = 800;
 		new LwjglApplication(new SuperMario(), config);
 	}
 }
